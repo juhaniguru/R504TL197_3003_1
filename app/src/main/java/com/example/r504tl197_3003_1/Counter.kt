6c846b4134c29520474e3377a62d5e0a57fcebe9
Kuru Juhani <juhani.kuru@lapinamk.fi>
2026-03-10 13:44:24 +0200

    - add remember + mutableStateOf

---
+++ app/src/main/java/com/example/r504tl197_3003_1/Counter.kt
@@ -0,0 +1,47 @@
+package com.example.r504tl197_3003_1
+
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+
+// constCounter on vakio arvo, joten sen arvoa ei voi muuttaa
+const val constCounter = 0
+
+// muutetaan niin, että counter on nyt mutableStateOf, joka saa arvokseen 0
+// voi halutessasi käyttää mutableIntStateOf:ia myös tässä. Compose ymmärtää, että count on integer arvo
+// mutableStateOfista on olemassa integereille oma overload (mutableIntStateOf())
+
+
+
+
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+    val count = remember {
+
+        mutableStateOf(0)
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+
+    ) {
+        Button(onClick = {
+            // kun muuttujan arvo laitetaan mutableStateOfiin, sitä ei voi suoraan enää kasvattaa
+            // vaan kasvatetaan sen valueta
+            count.value++
+
+        }) {
+            Text("count: ${count.value}")
+        }
+    }
+}
